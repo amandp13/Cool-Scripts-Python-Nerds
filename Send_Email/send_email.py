6915d2bae1164ebcca054bc976e5de21232c8496
Amir Naghibi <amir.naghibi@visier.com>
2020-10-26 17:10:33 -0700

    update method signature

--- Send_Email/send_email.py
+++ Send_Email/send_email.py
@@ -1,18 +1,12 @@
 import smtplib
 
 
-def send_email():
+def send_email(email, password, msg):
     server = smtplib.SMTP('smtp.gmail.com',587)
 
     server.starttls()
-
-    email = 'Enter Email Here'
-    password = 'Enter Password Here'
-
     server.login(email,password)
 
     sender = email
     reciever = email
-    msge = 'Enter Message Here'
-
-    server.sendmail(sender,reciever,msge)
+    server.sendmail(sender,reciever,msg)
