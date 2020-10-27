28f6f1bd47cc95473aff386f5944e73c89003ed7
Amir Naghibi <amir.naghibi@visier.com>
2020-10-26 17:04:17 -0700

    send email script

---
+++ Send_Email/send_email.py
@@ -0,0 +1,18 @@
+import smtplib
+
+
+def send_registration_email():
+    server = smtplib.SMTP('smtp.gmail.com',587)
+
+    server.starttls()
+
+    email = 'Enter Email Here'
+    password = 'Enter Password Here'
+
+    server.login(email,password)
+
+    sender = email
+    reciever = email
+    msge = 'Enter Message Here'
+
+    server.sendmail(sender,reciever,msge)
