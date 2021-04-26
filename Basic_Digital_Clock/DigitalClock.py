88caa8d1b8822b64c3cf7fcb08c280b89ef39db2
Restyled.io <commits@restyled.io>
2021-04-26 05:38:51 +0000

    Restyled by black

--- Basic_Digital_Clock/DigitalClock.py
+++ Basic_Digital_Clock/DigitalClock.py
@@ -12,14 +12,13 @@ root.title("Digital Clock")
 
 
 def time():
-    string = strftime('%H:%M:%S %p')
+    string = strftime("%H:%M:%S %p")
     label.config(text=string)
     label.after(1000, time)
 
 
-label = Label(root, font=("ds-digital", 80),
-              background="black", foreground="cyan")
-label.pack(anchor='center')
+label = Label(root, font=("ds-digital", 80), background="black", foreground="cyan")
+label.pack(anchor="center")
 time()
 
 mainloop()
