d4dad65c0777f398f28c390c54b45e196ddd4d4a
Restyled.io <commits@restyled.io>
2021-12-05 18:19:23 +0000

    Restyled by black

--- OpenCV_Projects/Face Recognition with Python/Create.py
+++ OpenCV_Projects/Face Recognition with Python/Create.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 import numpy
 import os
+
 s = input("Enter Roll No\n")
 haar_file = "haarcascade_frontalface_default.xml"
 datasets = "C:/users/wwwhs/Downloads"
@@ -14,17 +15,17 @@ if not os.path.isdir(path):
 face_cascade = cv2.CascadeClassifier(haar_file)
 webcam = cv2.VideoCapture(0)
 count = 1
-while(count < 30):
+while count < 30:
     (_, im) = webcam.read()
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
-    for(x, y, w, h) in faces:
-        cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        face = gray[y:y+h, x:x+w]
+    for (x, y, w, h) in faces:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = gray[y : y + h, x : x + w]
         face_resize = cv2.resize(face, (width, height))
-        cv2.imwrite('%s/%s.png' % (path, count), face_resize)
+        cv2.imwrite("%s/%s.png" % (path, count), face_resize)
     count += 1
-    cv2.imshow('OpenCV', im)
+    cv2.imshow("OpenCV", im)
     key = cv2.waitKey(10)
-    if(key == 27):
+    if key == 27:
         break
